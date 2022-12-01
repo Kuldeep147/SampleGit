f54e04adb5d85f321612256e2de374760a08e3aa
Kuldeep147 <kuldeep.bargurjar@gmail.com>
2022-12-01 20:10:22 +0530

    subtraction uc3

--- src/com/bzl/sample/SampleGit.java
+++ src/com/bzl/sample/SampleGit.java
@@ -6,9 +6,13 @@ public class SampleGit {
         System.out.println("welcome to sample git");
         SampleGit ob = new SampleGit();
         ob.add(3,8);
+        ob.subtract(45, 23);
 
     }
     void add(int a, int b){
         System.out.println("addition "+(a + b));
+    }
+    void subtract(int a, int b){
+        System.out.println("substraction : "+(a - b));
     }
 }
